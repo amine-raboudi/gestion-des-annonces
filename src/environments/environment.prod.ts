1f319176758b0aaf19acf5a4bd99f7bdb4d4fa5b
Ahdy Kefi <ahdykefi47@gmail.com>
2022-10-21 20:05:00 +0100

    auth+annonce

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -10,13 +10,3 @@ export const firebaseConfig = {
   appId: '1:971603638490:web:be70a7c47499377fbdcde3',
   measurementId: 'G-M92JX05G60',
 };
-
-export const firebaseConfig = {
-  apiKey: "AIzaSyDKCMeQraPzgqcb9MIPUd8d8DrnVQID_Xc",
-  authDomain: "gestion-des-annonces.firebaseapp.com",
-  projectId: "gestion-des-annonces",
-  storageBucket: "gestion-des-annonces.appspot.com",
-  messagingSenderId: "971603638490",
-  appId: "1:971603638490:web:be70a7c47499377fbdcde3",
-  measurementId: "G-M92JX05G60"
-};
