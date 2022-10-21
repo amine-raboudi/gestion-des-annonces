b9408c24c1620f6d3133c75a2e2ea5eb0f837038
dalibouali <boualimohamed77@gmail.com>
2022-10-21 19:20:37 +0100

    authentication done

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import * as firebase from 'firebase/app';
-import {HttpClient}  from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 
@@ -10,33 +10,40 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class AuthService {
- 
+
   ngFireAuth: any;
- 
 
-  constructor(public auth: AngularFireAuth,private httpClient:HttpClient) { }
-  
 
-  loginFireauth(value){
-    return new Promise<any> ( (resolve, reject)=>{
+  constructor(public auth: AngularFireAuth, private httpClient: HttpClient) { }
+
+
+  loginFireauth(value) {
+    return new Promise<any>((resolve, reject) => {
       this.auth.signInWithEmailAndPassword(value.email, value.password).then(
-        res => resolve(res),
+
+
+        res => {
+          resolve(res)
+          window.localStorage.removeItem('email');
+          window.localStorage.setItem('email', res.user.email)
+        }
+        ,
         error => reject(error)
       )
     })
-   }
+  }
 
-   
+
 
 
 
   RegisterUser(form) {
-     return this.auth.createUserWithEmailAndPassword(form.email, form.password);
-     
+    return this.auth.createUserWithEmailAndPassword(form.email, form.password);
+
   }
 
-  addProfile(user):Observable<any>{
-    return this.httpClient.post('https://gestion-des-annonces-default-rtdb.europe-west1.firebasedatabase.app/users.json',user)
+  addProfile(user): Observable<any> {
+    return this.httpClient.post('https://gestion-des-annonces-default-rtdb.europe-west1.firebasedatabase.app/users.json', user)
   }
 
 
